406545b588930222fb8f02823fd2e3d86c65fad3
LazTheArtist <kingofcreepersyt@gmail.com>
2021-05-04 15:59:03 +0200

    some progress on da cookbot gui

--- src/main/java/multiteam/scrapcraft/main/container/CookBotScreen.java
+++ src/main/java/multiteam/scrapcraft/main/container/CookBotScreen.java
@@ -3,11 +3,13 @@ package multiteam.scrapcraft.main.container;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import multiteam.scrapcraft.ScrapCraft;
+import multiteam.scrapcraft.main.block.ModBlocks;
+import multiteam.scrapcraft.main.block.cookbot.CookBotTileEntity;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.MouseHelper;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
-import net.minecraft.client.util.InputMappings;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector2f;
 import net.minecraft.util.math.vector.Vector4f;
@@ -15,9 +17,6 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-
-import javax.swing.plaf.basic.BasicListUI;
-import java.util.Vector;
 
 @OnlyIn(Dist.CLIENT)
 public class CookBotScreen extends ContainerScreen<CookBotContainer> {
@@ -27,6 +26,8 @@ public class CookBotScreen extends ContainerScreen<CookBotContainer> {
     private static final ResourceLocation COOKBOT_GUI = new ResourceLocation(ScrapCraft.MOD_ID, "textures/gui/cookbot_menu.png");
     public static final TranslationTextComponent COOKBOT_LABEL = new TranslationTextComponent("container."+ ScrapCraft.MOD_ID +".cookbot");
     public static final Vector4f FIELD_1 = new Vector4f(7.0f,85.0f,60.0f,116.0f);
+    public static final Vector4f FIELD_2 = new Vector4f(90.0f,168.0f,60.0f,116.0f);
+    public static final Vector4f FIELD_3 = new Vector4f(7.0f,168.0f,119.0f,175.0f);
     public static int selectedFood = 1;
 
     public CookBotScreen(CookBotContainer container, PlayerInventory playerInv, ITextComponent title) {
@@ -38,15 +39,6 @@ public class CookBotScreen extends ContainerScreen<CookBotContainer> {
         this.leftPos = 0;
         this.topPos = 0;
     }
-
-    /**@Override
-    protected void init() {
-        int relX = (this.width - WIDTH) / 2;
-        int relY = (this.height - HEIGHT) / 2;
-
-        addButton(new Button(relX + 10, relY + 118, 160, 20, new StringTextComponent("Chicken"), button -> codeThatsRunByButtonPress("minecraft:chicken")));
-
-    }**/
 
     private void codeThatsRunByButtonPress(String id) {
         System.out.println(id);
@@ -65,7 +57,7 @@ public class CookBotScreen extends ContainerScreen<CookBotContainer> {
     }
 
     @Override
-    protected void renderBg(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY) {
+    public void renderBg(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY) {
         Vector2f mousePos = new Vector2f(mouseX, mouseY);
         int offsetX = (this.width - WIDTH) / 2;
         int offsetY = (this.height - HEIGHT) / 2;
@@ -82,38 +74,40 @@ public class CookBotScreen extends ContainerScreen<CookBotContainer> {
 
         //Live stuff
         this.minecraft.textureManager.bind(COOKBOT_GUI);
-        switch (selectedFood){
+        /**switch (selectedFood){
             case 1 :
-                this.blit(matrixStack, 7+offsetX, 60+offsetY, this.getBlitOffset(), 2.0F, 245.0F, 80, 57, 512, 512);
+                selectFied(1, matrixStack, offsetX, offsetY);
                 break;
             case 2 :
-                this.blit(matrixStack, 90+offsetX, 60+offsetY, this.getBlitOffset(), 85.0F, 245.0F, 163, 57, 512, 512);
+                selectFied(2, matrixStack, offsetX, offsetY);
                 break;
             case 3 :
-                this.blit(matrixStack, 7+offsetX, 119+offsetY, this.getBlitOffset(), 2.0F, 304.0F, 163, 57, 512, 512);
+                selectFied(3, matrixStack, offsetX, offsetY);
                 break;
-        }
+        }**/
 
+        //Render Cookbot model - needs rework.
+        this.minecraft.getItemRenderer().renderGuiItem(new ItemStack(ModBlocks.COOKBOT_BLOCK.get().asItem()), offsetX, offsetY);
+
+        //IRenderTypeBuffer.Impl irendertypebuffer$impl = Minecraft.getInstance().renderBuffers().bufferSource();
+        //this.minecraft.getEntityRenderDispatcher().render(this.cookBotTile,0.0D, 0.0D, 0.0D, 0.0F, 1.0F, matrixStack, irendertypebuffer$impl, 15728880);
+        //InventoryScreen.renderEntityInInventory(offsetX, offsetY, 1, mouseX, mouseY, this.cookBotTile);
+        //CookBotRenderer red = new CookBotRenderer(Minecraft.getInstance().);
+
+
+        //Field-Hover and click detection for selecting
         if(isMouseInsideBox(mousePos, FIELD_1, offsetX, offsetY)){
-
+            selectFied(1, matrixStack, offsetX, offsetY);
         }
-
-        System.out.println(mouseClicked(0,0,0));
-        System.out.println(mouseClicked(1,1,1));
-        System.out.println(mouseClicked(2,2,2));
-        System.out.println(mouseClicked(3,3,3));
-
+        if(isMouseInsideBox(mousePos, FIELD_2, offsetX, offsetY)){
+            selectFied(2, matrixStack, offsetX, offsetY);
+        }
+        if(isMouseInsideBox(mousePos, FIELD_3, offsetX, offsetY)){
+            selectFied(3, matrixStack, offsetX, offsetY);
+        }
 
     }
 
-    public boolean isMouseInsideBox(int mouseX, int mouseY, int minX, int maxX, int minY, int maxY){
-        if(mouseX > minX && mouseX < maxX){
-            if(mouseY > minY && mouseY < maxY){
-                return true;
-            }
-        }
-        return false;
-    }
 
     //This should go into MultiCoreLib...
     public boolean isMouseInsideBox(Vector2f mousPos, Vector4f box, int offsetX, int offsetY){
@@ -123,5 +117,22 @@ public class CookBotScreen extends ContainerScreen<CookBotContainer> {
             }
         }
         return false;
+    }
+
+    public void selectFied(int fieldId, MatrixStack matrixStack, int offsetX, int offsetY){
+        switch (fieldId){
+            case 1 :
+                this.minecraft.textureManager.bind(COOKBOT_GUI);
+                this.blit(matrixStack, 7+offsetX, 60+offsetY, this.getBlitOffset(), 2.0F, 245.0F, 80, 57, 512, 512);
+                break;
+            case 2 :
+                this.minecraft.textureManager.bind(COOKBOT_GUI);
+                this.blit(matrixStack, 90+offsetX, 60+offsetY, this.getBlitOffset(), 85.0F, 245.0F, 80, 57, 512, 512);
+                break;
+            case 3 :
+                this.minecraft.textureManager.bind(COOKBOT_GUI);
+                this.blit(matrixStack, 7+offsetX, 119+offsetY, this.getBlitOffset(), 2.0F, 304.0F, 163, 57, 512, 512);
+                break;
+        }
     }
 }
